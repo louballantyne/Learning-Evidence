47453ec5cc71a76cea6ab1e541410f5b54bcd58f
Louise Ballantyne <lou@birdline.co.uk>
2021-03-02 12:00:01 +0000

    Added exercises for Cohesion and Encapsulation

---
+++ OOP/Encapsulation.rb
@@ -0,0 +1,17 @@
+class PrintAnswer
+  def print_answer(answer)
+    "The Answer is: #{answer }"
+  end
+end
+
+class Operations
+  def add(number_1, number_2)
+    answer = number_1 + number_2
+    return answer
+  end
+
+  def subtract(number_1, number_2)
+    answer = number_1 - number_2
+    return answer
+  end
+end
---
+++ OOP/secret-diary.rb
@@ -0,0 +1,33 @@
+#Initially the `SecretDiary` class is locked, meaning `add_entry` and `get_entries`
+#should throw an error.
+
+#When the user calls `unlock`, `add_entry` and `get_entries` should work as desired.
+
+#When the user locks the diary, by calling `lock`, `add_entry` and `get_entries` should,
+#again, throw an error.
+
+class SecretDiary
+
+  def initialize
+    @lock = true
+    @entries = []
+  end
+
+  def add_entry(entry)
+    return "Diary locked" if @lock
+    @entries << entry
+  end
+
+  def get_entries
+    @lock ? "Diary locked" : @entries
+  end
+
+  def unlock
+    @lock = false
+  end
+
+  def lock
+    @lock = true
+  end
+
+end
